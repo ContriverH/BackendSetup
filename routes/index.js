3744fdd7179d098e079f6ead28644f091ec030b3
Himanshu Pal <himanshu.p19@iiits.in>
2021-08-27 01:49:18 +0530

    adding layout and modules

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const routes = express.Router();
+const router = express.Router();
 
-routes.get("/", (req, res) => {
-  res.send("Hello World");
+router.get("/", (req, res) => {
+  //   res.send("Hello World");
+  res.render("index"); // It will render the file in the views
 }); // this alone is not going to run the server at this route. This is because the server does not know unless we require it in the server.js file and let it know that this route exists
+
+module.exports = router; // this is the way to export any variable
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+if (process.env.NODE_ENV !== "production") {
+  // this is done because we want dotenv to only load in the development environment
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express(); // to get the app portion of express
 const expressLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index"); // this will tell the location of our routes
 
 // configuring our express application
 app.set("view engine", "ejs"); // we want to set the view engine as ejs
@@ -9,5 +16,16 @@ app.set("layout", "layouts/layout"); // we can tell how our layout files are goi
 // the idea behind the layour files is that every single file is going to be put inside of this layout file, so that we don't have to duplicate all the begining and ending html of the project such as header and footer
 app.use(expressLayouts); // tell node that we want to use the expressLayouts
 app.use(express.static("public")); // this will tell where our public files are going to be like, stylesheets, js files, images
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATBASE_URL, { useNewUrlPraser: true }); // DATABASE_URL is being provided by the environment variable when using server, but for local use we need to create a .env file and specify the variable over there otherwise it will through error. Now the DATABASE_URL is in .env file
+//adding some options to how we want to setup our application in mongoDB
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error)); // this is to check whether we are connected to our database
+db.once("open", () => console.log("Connected to Mongoose")); // this is going to run only once when we are connected to mongoose
+
+app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 3000); // this is going to pull the port from the enviornment variable, the server is going to tell what source it is listening to but for the development purposes we assign a port number by ourselves since the server is not telling anything, so we set the port to 3000
