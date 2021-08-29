783217a23fdea4e7e803edef0dc8f540e7f1d3bd
Himanshu Pal <himanshu.p19@iiits.in>
2021-08-29 10:48:19 +0530

    adding new author's functionality

--- routes/authors.js
+++ routes/authors.js
@@ -4,8 +4,14 @@ const Author = require("../models/author");
 
 // All authors route
 // since in the server.js using the command-> use("./authors"),  so we can directly start writing from / while specifing the route
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   res.render("authors/index"); // authors/index.ejs is the file location
+  try {
+    const authors = await Author.find({});
+    res.render("authors/index", { authors: authors });
+  } catch {
+    res.redirect("/");
+  }
 });
 
 // new authors route
@@ -15,20 +21,31 @@ router.get("/new", (req, res) => {
 
 // create authors route
 // its working starts on getting the post request and most probabily from the forms.
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const author = new Author({ name: req.body.name });
   // we need to make sure that we do not pass the whole body inside the new Author() object. This is because body may have id in it, which will change the id we have already assigned. This will make unwanted changes to the model. So, to avoid that we will accept and pass only the attribute that we need to change or modify in the model.
-  author.save((err, newAuthor) => {
-    if (err) {
-      res.render("authors/new", {
-        author: author, // in case if the user has entered the name of the author, so he does not have to enter it again.
-        errorMessage: "Error creating Author",
-      });
-    } else {
-      // res.redirect(`authors /${newAuthor.id}`); we will use it when we create the newAuthor page
-      res.redirect(`authors`);
-    }
-  });
+
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors /${newAuthor.id}`); we will use it when we create the newAuthor page
+    res.redirect(`authors`);
+  } catch {
+    res.render("authors/new", {
+      author: author, // in case if the user has entered the name of the author, so he does not have to enter it again.
+      errorMessage: "Error creating Author",
+    });
+  }
+  // author.save((err, newAuthor) => {
+  //   if (err) {
+  //     res.render("authors/new", {
+  //       author: author, // in case if the user has entered the name of the author, so he does not have to enter it again.
+  //       errorMessage: "Error creating Author",
+  //     });
+  //   } else {
+  //     // res.redirect(`authors /${newAuthor.id}`); we will use it when we create the newAuthor page
+  //     res.redirect(`authors`);
+  //   }
+  // });
 });
 
 module.exports = router;
